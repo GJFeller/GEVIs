414b4fd3bff72401ec180af9cf796c8c24aefda2
Gustavo Jandt Feller <fellergustavo@gmail.com>
2017-12-03 00:24:30 -0200

    First code commit

---
+++ javascript/windows-system.js
@@ -0,0 +1,129 @@
+/*
+ * Created by Rodrigo on 25/05/2017.
+ */
+
+/* Initial values of panel Height and Width */
+var INITIAL_HEIGHT = 250;
+var INITIAL_WIDTH = 350;
+
+/* Max values of panel Height and Width */
+var MAX_HEIGHT = 620;
+var MAX_WIDTH = 1000;
+
+/* Constant values of icon Height and Width */
+var HEIGHT_ICON = 28;
+var WIDTH_ICON = 28;
+
+/* Creating the tree with the first node */
+var tree;//new Tree('panel-1-1');
+
+function initWindow() {
+    createNewChild("panel-1-1", "variable-selection", true);
+}
+
+function createNewChild(currentId, chartObj, isRoot) {
+    var newElem = $('<div '+ 'id="' + currentId + '" class="panel panel-default"> <div class="panel-heading clearfix"> <h4 class="panel-title pull-left" style="padding-top: 7.5px;">' + chartObj + '</h4> <button disabled class="btn btn-default btn-remove"><i class="glyphicon glyphicon-remove"></i></button> <button class="btn btn-default btn-minimize"><i class="glyphicon glyphicon-minus"></i></button> </div><div class="panel-body center-panel"></div></div>').css({"position": "absolute"});
+    //var newID = "";
+    var chart;
+    $(".container").append(newElem);
+
+    if(isRoot) {
+        //tree = new Tree(currentId);
+    }
+
+    /* Sets up the panel settings as drag, resize, etc */
+    setUpPanel(currentId);
+
+}
+
+function setUpPanel(newID) {
+    /* Guarantees the right colors of btn-minimize */
+    $("#"+ newID + " .btn-default.btn-minimize")
+        .mouseenter(function() {
+            $(this).css("background", "#e6e6e6");
+        })
+        .mouseleave(function() {
+            $(this).css("background", "#fff");
+        });
+
+    /* Getting the workspace SVG */
+    var workspace = $("#workspace");
+
+    //var isTimeline = newID === "panel-1-1";
+
+    var initialWidth, initialHeight, minWidth, minHeight, maxWidth, maxHeight;
+
+    initialWidth = INITIAL_WIDTH;
+    initialHeight = INITIAL_HEIGHT;
+    minWidth = INITIAL_WIDTH;
+    minHeight = INITIAL_HEIGHT;
+    maxWidth  = MAX_WIDTH;
+    maxHeight = MAX_HEIGHT;
+
+    /* Setting up the panel */
+    $( "#" + newID)
+        .draggable({
+            handle: ".panel-heading",
+            stack: ".panel, .fa-window-maximize",
+            containment: [10,10, workspace.width() - initialWidth - 10 , 
+                workspace.height() - initialHeight - 90],
+            drag: function(){
+                //centerLine(this.id);
+            },
+            cancel: '.dropdown-menu'
+        })
+        .find(".panel-body")
+        .css({
+            height: initialHeight,
+            width: initialWidth
+        })
+        .resizable({
+            resize: function(){
+                //var aPanel = $(this).parents(".panel")[0];
+                //centerLine(aPanel.id);
+            },
+            aspectRatio: true,
+            maxHeight: maxHeight,
+            maxWidth: maxWidth,
+            minHeight: minHeight,
+            minWidth: minWidth
+        });
+}
+
+function centerLine(panelID, icon) {
+    if (typeof icon === 'undefined') { icon = false; }
+    var lines =  d3.selectAll("line").filter(".class-" + panelID);
+    var sizeLines = lines.size();
+
+    for (var i = 0; i < sizeLines; i++)
+    {
+        var aLine = $("#" + lines[0][i].id);
+        var lineID = lines[0][i].id.split("_");
+        if (lineID[0] === panelID)
+        {
+            if (!icon)
+            {
+                aLine.attr("x1", getCenter(lineID[0])["x"]);
+                aLine.attr("y1", getCenter(lineID[0])["y"]);
+            }
+            else
+            {
+                aLine.attr("x1", parseInt(getCenter("icon-" + lineID[0])["x"]));
+                aLine.attr("y1", parseInt(getCenter("icon-" + lineID[0])["y"]));
+            }
+        }
+        else
+        {
+            if (!icon)
+            {
+                aLine.attr("x2", getCenter(lineID[1])["x"]);
+                aLine.attr("y2", getCenter(lineID[1])["y"]);
+            }
+            else
+            {
+                aLine.attr("x2", parseInt(getCenter("icon-" + lineID[1])["x"]));
+                aLine.attr("y2", parseInt(getCenter("icon-" + lineID[1])["y"]));
+            }
+        }
+    }
+}
