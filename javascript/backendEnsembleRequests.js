f958956ba827a5b5d91a6e92611f0344d86904a5
Gustavo Feller <fellergustavo@gmail.com>
2017-12-21 02:23:51 -0200

    Getting requests from backend

--- javascript/backendEnsembleRequests.js
+++ javascript/backendEnsembleRequests.js
@@ -4,33 +4,68 @@ class BackendRequests {
     }
 
     getListEnsembles() {
-        var xhttp = new XMLHttpRequest();
-        xhttp.open("GET", this.url + "/getAllEnsembles", true);
-        xhttp.send();
+        var $this = this;
+        return new Promise(function(resolve, reject) {
+            var xhttp = new XMLHttpRequest();
+            xhttp.onload = function() {
+                resolve(JSON.parse(this.responseText));
+            };
+            xhttp.onerror = reject;
+            xhttp.open("GET", $this.url + "/getAllEnsembles", true);
+            xhttp.send();
+        });    
     }
 
     getEnsembleVariables(ensembleId) {
-        var xhttp = new XMLHttpRequest();
-        xhttp.open("GET", this.url + "/getVariablesEnsemble/ensembleId="+ensembleId, true);
-        xhttp.send();
+        var $this = this;
+        return new Promise(function(resolve, reject) {
+            var xhttp = new XMLHttpRequest();
+            xhttp.onload = function() {
+                resolve(JSON.parse(this.responseText));
+            };
+            xhttp.onerror = reject;
+            xhttp.open("GET", $this.url + "/getVariablesEnsemble?ensembleId="+ensembleId, true);
+            xhttp.send();
+        });
     }
 
     getAllVariables(ensembleId) {
-        var xhttp = new XMLHttpRequest();
-        xhttp.open("GET", this.url + "/getAllVariables/ensembleId="+ensembleId, true);
-        xhttp.send();
+        var $this = this;
+        return new Promise(function(resolve, reject) {
+            var xhttp = new XMLHttpRequest();
+            xhttp.onload = function() {
+                resolve(JSON.parse(this.responseText));
+            };
+            xhttp.onerror = reject;
+            xhttp.open("GET", $this.url + "/getAllVariables?ensembleId="+ensembleId, true);
+            xhttp.send();
+        });  
     }
 
     getTemporalData(xIdx, yIdx, zIdx, simulationId, varId, ensembleId) {
-        var xhttp = new XMLHttpRequest();
-        xhttp.open("GET", this.url + "/getTemporalVarData/xIdx="+xIdx+"&yIdx="+yIdx+"&zIdx="+zIdx+"&simulationId="+simulationId+"&varId="+varId+"&ensembleId="+ensembleId, true);
-        xhttp.send();
+        var $this = this;
+        return new Promise(function(resolve, reject) {
+            var xhttp = new XMLHttpRequest();
+            xhttp.onload = function() {
+                resolve(JSON.parse(this.responseText));
+            };
+            xhttp.onerror = reject;
+            xhttp.open("GET", $this.url + "/getTemporalVarData?xIdx="+xIdx+"&yIdx="+yIdx+"&zIdx="+zIdx+"&simulationId="+simulationId+"&varId="+varId+"&ensembleId="+ensembleId, true);
+            xhttp.send();
+        });  
     }
 
     getMultivariateData(xIdx, yIdx, zIdx, time, simulationId, varIdList) {
-        var xhttp = new XMLHttpRequest();
-        xhttp.open("GET", this.url + "/getTemporalVarData/xIdx="+xIdx+"&yIdx="+yIdx+"&zIdx="+zIdx+"&time="+time+"&simulationId="+simulationId+"&varIdList="+varIdList, true);
-        xhttp.send();
+        var $this = this;
+        return new Promise(function(resolve, reject) {
+            var xhttp = new XMLHttpRequest();
+            xhttp.onload = function() {
+                resolve(JSON.parse(this.responseText));
+            };
+            xhttp.onerror = reject;
+            xhttp.open("GET", $this.url + "/getTemporalVarData?xIdx="+xIdx+"&yIdx="+yIdx+"&zIdx="+zIdx+"&time="+time+"&simulationId="+simulationId+"&varIdList="+varIdList, true);
+            xhttp.send();
+        });  
     }
 
 
