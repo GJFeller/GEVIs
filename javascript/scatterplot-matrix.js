abd2df8a36fe293c6be42f63c8ec6eaad529f30e
gjfeller <fellergustavo@gmail.com>
2018-01-02 01:15:23 -0200

    Formating multivariate data for the scatterplot code

--- javascript/scatterplot-matrix.js
+++ javascript/scatterplot-matrix.js
@@ -14,9 +14,14 @@ class ScatterplotMatrix extends AbstractPanelBuilder {
     }
 
     getRemoteData() {
+        var $this = this;
         if(this.data instanceof Array)
             this.data.splice(0,this.data.length);
-            
+
+        var varNameList = [];
+        this.varList.forEach(function (variable, idx) {
+            varNameList[idx] = variable.variable + "-" + variable.specie;
+        });
         var simulationList = selectVariablesPanel.getEnsembleList()[0].simulations;
         var promises = [];
         for(var i = 0; i < simulationList.length; i++) {
@@ -31,14 +36,31 @@ class ScatterplotMatrix extends AbstractPanelBuilder {
         }
         Promise.all(promises)
             .then(function(values) {
-                console.log(values);
+                var dataList = [];
+                values.forEach(function (elem) {
+                    if(elem.length > 0)
+                    {
+                        var data = {};
+                        data.simulationId = elem[0].simulationId;
+                        data.time = elem[0].time;
+                        $this.varList.forEach(function (variable, idx) {
+                            for(var i = 0; i < elem[0].variables.length; i++) {
+                                if(variable.id == elem[0].variables[i].variableId) {
+                                    data[varNameList[idx]] = elem[0].variables[i].value;
+                                }
+                            }
+                        });
+                        dataList.push(data);
+                    }
+                });
+                $this.data = dataList;
+                $this.render();
             });
     }
 
     setVariableList(varList) {
         this.varList = varList;
         this.getRemoteData();
-        this.render();
     }
 
     render() {
@@ -46,9 +68,12 @@ class ScatterplotMatrix extends AbstractPanelBuilder {
         var height = this.panel.height();
         var margin = {top: 30, right: 30, bottom: 30, left: 50};
         var padding = 20;
-        var numberVariables = 4;
-        var sizeW = width / numberVariables;
-        var sizeH = height / numberVariables;
+        console.log(this.data);
+        /*var numberVariables = 1;
+        if(this.data.length > 0)
+            numberVariables = this.data[0].variables.length;
+        var sizeW = (width / numberVariables) - (numberVariables-1)*padding - margin.right - margin.left;
+        var sizeH = (height / numberVariables) - (numberVariables-1)*padding - margin.top - margin.bottom;
 
         var x = d3.scale.linear()
             .range([padding / 2, sizeW - padding / 2]);
@@ -78,9 +103,18 @@ class ScatterplotMatrix extends AbstractPanelBuilder {
             .on("brush", this.brushmove)
             .on("brushend", this.brushend);
 
+        // Position scales.
+        var position = {};
+        /*flower.traits.forEach(function(trait) {
+        function value(d) { return d[trait]; }
+        position[trait] = d3.scale.linear()
+            .domain([d3.min(flower.values, value), d3.max(flower.values, value)])
+            .range([padding / 2, size - padding / 2]);
+        });
+
         var svg = d3.select("#"+this.id+"-scatter");
         svg.append("g")
-            .attr("transform", "translate(" + padding + margin.left + "," + padding / 2 + ")")
+            .attr("transform", "translate(" + padding + margin.left + "," + padding / 2 + ")")*/
 
     }
 
