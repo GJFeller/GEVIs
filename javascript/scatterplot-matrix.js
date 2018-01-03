428a096632a5d69f1f6e3c2be132cb46247230ae
gjfeller <fellergustavo@gmail.com>
2018-01-02 22:18:08 -0200

    Added the scatter plot matrix

--- javascript/scatterplot-matrix.js
+++ javascript/scatterplot-matrix.js
@@ -64,11 +64,251 @@ class ScatterplotMatrix extends AbstractPanelBuilder {
     }
 
     render() {
+        var $this = this;
         var width = this.panel.width();
         var height = this.panel.height();
         var margin = {top: 30, right: 30, bottom: 30, left: 50};
         var padding = 20;
-        console.log(this.data);
+        //console.log(this.data);
+        if(this.data.length > 0) {
+            var domainByVariable = {},
+                variables = d3.keys(this.data[0]).filter(function (d) { return d !== "simulationId" && d !== "time"; }),
+                numberVariables = variables.length;
+            
+            // Get Min and Max of each of the columns
+            variables.forEach(function(variable) {
+                domainByVariable[variable] = d3.extent($this.data, function(d) {
+                    return d[variable];
+                });
+            });
+
+            var size = 0;
+            if(height < width) {
+                size = height / numberVariables;
+            }
+            else {
+                size = width / numberVariables;
+            }
+
+            var formatSiPrefix = d3.format("3e");
+
+            var x = d3.scale.linear()
+                .range([padding / 2, size - padding / 2]);
+            
+            var y = d3.scale.linear()
+                .range([size - padding / 2, padding / 2]);
+            
+            var xAxis = d3.svg.axis()
+                .scale(x)
+                .orient("bottom")
+                .ticks(6)
+                .tickFormat(formatSiPrefix);
+
+            var yAxis = d3.svg.axis()
+                .scale(y)
+                .orient("left")
+                .ticks(6)
+                .tickFormat(formatSiPrefix);
+
+            var brush = d3.svg.brush()
+                .x(x)
+                .y(y)
+                .on("brushstart", brushstart)
+                .on("brush", brushmove)
+                .on("brushend", brushend);;
+            
+            var svg = d3.select("#"+this.id+"-scatter");
+
+            svg.selectAll('*').remove();
+
+            svg.attr("width", size * numberVariables + padding + margin.left + margin.right)
+                .attr("height", size * numberVariables + padding + margin.top + margin.bottom)
+            .append("g")
+                .attr("transform", "translate(" + padding + "," + padding / 2 + ")");
+            
+            // Set the ticks to stretch across all plots
+            xAxis.tickSize(size * numberVariables);
+            yAxis.tickSize(-size * numberVariables); // negative so ticks go right
+
+            svg.selectAll(".x.axisSPLOTM")
+                .data(variables)
+            .enter().append("g")
+                .attr("class", "x axisSPLOTM")
+                .attr("transform", function(d, i) { return "translate(" + ((i) * size + margin.left) + ",0)";})
+                .each(function(d) { x.domain(domainByVariable[d]); d3.select(this).call(xAxis); });
+
+            svg.selectAll(".y.axisSPLOTM")
+                .data(variables)
+            .enter().append("g")
+                .attr("class", "y axisSPLOTM")
+                .attr("transform", function(d, i) { return "translate(" + margin.left + "," + i * size + ")";})
+                .each(function(d) { y.domain(domainByVariable[d]); d3.select(this).call(yAxis); });
+
+            var cell = svg.selectAll(".cellSPLOTM")
+                .data(cross(variables, variables))
+            .enter().append("g")
+                .attr("class", "cellSPLOTM")
+                .attr("transform", function(d) { return "translate(" + ((d.i) * size + margin.left) + "," + d.j * size + ")"; })
+                .each(plot);
+
+            // Titles for the diagonal.
+            cell.filter(function(d) { return d.i === d.j; }).append("text")
+                .attr("x", padding)
+                .attr("y", padding)
+                .attr("dy", ".71em")
+                .text(function(d) { return d.x; });
+
+            // Create each x-axis
+            /*svg.selectAll(".x.axis")
+                .data(variables)
+            .enter().append("g")
+                .attr("class", "x axis")
+                .attr("transform", function(d, i) { return "translate(" + (numberVariables - i - 1) * size + ",0)"; })
+                .each(function(d) { 
+                    x.domain(domainByVariable[d]);
+                    d3.select(this).call(xAxis);
+                });
+
+            // Create each y-axis
+            svg.selectAll(".y.axis")
+                .data(variables)
+            .enter().append("g")
+                .attr("class", "y axis")
+                .attr("transform", function(d, i) { 
+                    return "translate(0," + i * size + ")";
+                })
+                .each(function(d) {
+                    y.domain(domainByVariable[d]);
+                    d3.select(this).call(yAxis);
+                });
+            
+            var cell = svg.selectAll(".cell")
+                .data(cross(variables, variables))
+                .enter().append("g")
+                .attr("class", "cell") ;
+          
+            cell.filter(function(d) { return d.i !== d.j; })
+                .attr("transform", function(d) { return "translate(" + (numberVariables - d.i - 1) * size + "," + d.j * size + ")"; })
+                .each(plot);
+          
+            cell.filter(function(d) { return d.i === d.j; })
+                .attr("transform", function(d) { return "translate(" + (numberVariables - d.i - 1) * size + "," + d.j * size + ")"; })
+                .each(plotHistogram);
+          
+            // Titles for the diagonal.
+            cell.filter(function(d) { return d.i === d.j; }).append("text")
+                .attr("x", padding)
+                .attr("y", padding)
+                .attr("dy", ".71em")
+            .text(function(d) { return d.x; });*/
+
+            // Run the brush
+            cell.call(brush);
+
+            function plot(p) {
+                var cell = d3.select(this);
+                
+                x.domain(domainByVariable[p.x]);
+                y.domain(domainByVariable[p.y]);
+                
+                cell.append("rect")
+                    .attr("class", "frameSPLOTM")
+                    .attr("x", padding / 2)
+                    .attr("y", padding / 2)
+                    .attr("width", size - padding)
+                    .attr("height", size - padding);
+                    //.style("fill", function(d) { return d3.rgb(255,255,255); });
+                
+                cell.selectAll("circle")
+                    .data($this.data)
+                .enter().append("circle")
+                    .attr("cx", function(d) { return x(d[p.x]); })
+                    .attr("cy", function(d) { return y(d[p.y]); })
+                    .attr("r", 4)
+                    .attr("test-x", function(d) {return d[p.x]; })
+                    .attr("test-y", function(d) {return d[p.y]; })
+                    .style("fill", function(d) { return d3.rgb(0,0,255); });
+            }
+
+            function plotHistogram(p) {
+                var cell = d3.select(this);
+            
+                x.domain(domainByVariable[p.x]);
+                y.domain(domainByVariable[p.y]);
+            
+                cell.append("rect")
+                    .attr("x", padding / 2)
+                    .attr("y", padding / 2)
+                    .attr("width", size - padding)
+                    .attr("height", size - padding)
+                    .attr("fill", "white")
+                    .attr("stroke","#aaa");
+            
+                // Extract data for histogramming into single array
+                var histData = $this.data.map(function(d) {
+                  return +d[p.x] ;
+                });
+            
+                // Generate a histogram using twenty uniformly-spaced bins.
+                var hist = d3.layout.histogram()
+                  .bins(x.ticks(20))
+                  (histData);
+            
+                var histScale = d3.scale.linear()
+                .domain([0, d3.max(hist, function(d) { return d.y; })])
+                .range([size - padding / 2, padding / 2]);
+            
+                var bar = cell.selectAll(".bar")
+                  .data(hist)
+                  .enter().append("g")
+                  .attr("class", "bar")
+                  .classed("histogram",true)
+                  .attr("transform", function(d) {
+                    return "translate(" + x(d.x) + "," + histScale(d.y) + ")";
+                  });
+            
+                bar.append("rect")
+                .attr("x", 1)
+                .attr("width", 5) //x(hist[0].dx) )
+                .attr("height", function(d) {
+                  return size - padding / 2 - histScale(d.y);
+                });
+            }
+
+            var brushCell;
+            
+            // Clear the previously-active brush, if any.
+            function brushstart(p) {
+                if (brushCell !== this) {
+                    d3.select(brushCell).call(brush.clear());
+                    x.domain(domainByVariable[p.x]);
+                    y.domain(domainByVariable[p.y]);
+                    brushCell = this;
+                }
+            }
+            
+            // Highlight the selected circles.
+            function brushmove(p) {
+                var e = brush.extent();
+                /*svg.selectAll("circle").classed("hiddenSPLOTM", function(d) {
+                    d.hidden = e[0][0] > d[p.x] || d[p.x] > e[1][0]
+                    || e[0][1] > d[p.y] || d[p.y] > e[1][1];
+                    return d.hidden;
+                });*/
+            }
+            
+            // If the brush is empty, select all circles.
+            function brushend() {
+                if (brush.empty()) svg.selectAll(".hiddenSPLOTM").classed("hiddenSPLOTM", false);
+            }
+
+            function cross(a, b) {
+                var c = [], n = a.length, m = b.length, i, j;
+                for (i = -1; ++i < n;) for (j = -1; ++j < m;) c.push({x: a[i], i: i, y: b[j], j: j});
+                return c;
+            }
+
+        }
         /*var numberVariables = 1;
         if(this.data.length > 0)
             numberVariables = this.data[0].variables.length;
@@ -131,8 +371,7 @@ class ScatterplotMatrix extends AbstractPanelBuilder {
     }
 
     resizePanel(width, height) {
-        console.log(width);
-        console.log(height);
+
     }
 
 }
